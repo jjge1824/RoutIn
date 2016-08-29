ba7a537a9be91662f3483dcf347e8272b8469dab
jose_jesus_guzman <jose1824@live.com.mx>
2016-08-28 21:00:03 -0500

    Build 0.0.17 Mapas agregados, Ui de principal cambiada, en curso la implementación de la localización y el pulimiento de los mapas.

--- app/src/main/java/com/example/jose_jesus_guzman/avanti/ClasesViews/PrincipalActivity.java
+++ app/src/main/java/com/example/jose_jesus_guzman/avanti/ClasesViews/PrincipalActivity.java
@@ -1,9 +1,18 @@
 package com.example.jose_jesus_guzman.avanti.ClasesViews;
 
+import android.Manifest;
+import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentManager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,11 +22,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.jose_jesus_guzman.avanti.R;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class PrincipalActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener,
+        OnMapReadyCallback,
+        android.location.LocationListener {
+
+    private double lattitud;
+    private double longitud;
+
+    private GoogleMap googleMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +70,81 @@ public class PrincipalActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        preguntarGooglePlay();
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        //Mostrar el mapa en la aplicación adecuadamente
+        SupportMapFragment supportMapFragment = (SupportMapFragment) fragmentManager.findFragmentById(R.id.map_fragment);
+        supportMapFragment.getMapAsync(this);
+
+        if (googleMap != null) {
+            googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            googleMap.setMyLocationEnabled(true);
+
+        } else {
+            Toast.makeText(PrincipalActivity.this, "Error Google Maps", Toast.LENGTH_LONG).show();
+        }
+
+        //Actualizar la unicacion mientras se mueve el dispositivo
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);//proveedor
+        if (location != null) {
+            showLocation(location.getLatitude(), location.getLongitude());
+        }
+
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                CameraUpdate camaraCentro = CameraUpdateFactory.newLatLng(new LatLng(location.getLatitude(), location.getLongitude()));
+                googleMap.moveCamera(camaraCentro);
+                showLocation(location.getLatitude(), location.getLongitude());
+
+            }
+        };
+
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);//PRecicion de la ubicacion
+        criteria.setAltitudeRequired(true);
+        criteria.setCostAllowed(false);//Si se pueden usar los datos
+        criteria.setSpeedRequired(true);
+        //Para buscar al mejor proveedor
+        String provider = locationManager.getBestProvider(criteria, true);
+        if (provider != null) {
+            locationManager.requestLocationUpdates(provider, 1000, 70, this);//Provider, rango en smilisegundos. rango en metros, locationListener
+        }
+    }
+
+    private void preguntarGooglePlay() {
+        //PReguntar si los servicios de google se encuentran activos
+        int codigoGooglePlay = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (codigoGooglePlay != ConnectionResult.SUCCESS) {
+            Dialog dialog = GooglePlayServicesUtil.getErrorDialog(codigoGooglePlay, this, 6);
+            if (dialog != null) {
+                dialog.show();
+            } else {
+                Toast.makeText(PrincipalActivity.this, "Error al verificar Google Play", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        }
+    }
+
+    private void showLocation(double latitud, double longitud) {
+        googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(latitud, longitud))
+                .title("Estoy aqui :3"));
     }
 
     @Override
@@ -64,18 +166,10 @@ public class PrincipalActivity extends AppCompatActivity
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.action_perfil:
-                startActivity(new Intent(PrincipalActivity.this, PerfilActivity.class));
-                break;
-            case R.id.action_acerca:
+            case R.id.action_proximos:
                 //TODO Accion de acerca de
 
                 startActivity(new Intent(PrincipalActivity.this, AcercaActivity.class));
-                break;
-            case R.id.action_cerrar:
-                //TODO Aciión de cerrar sesión
-
-                startActivity(new Intent(PrincipalActivity.this, PantallaInicialActivity.class));
                 break;
         }
         return super.onOptionsItemSelected(item);
@@ -86,7 +180,7 @@ public class PrincipalActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-
+/*
         if (id == R.id.nav_camera) {
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
@@ -100,9 +194,34 @@ public class PrincipalActivity extends AppCompatActivity
         } else if (id == R.id.nav_send) {
 
         }
-
+*/
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
     }
 }
